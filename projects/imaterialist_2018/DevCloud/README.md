166c399bc9028a5f431880d7f7367189e5430518
alkari <al@alkari.com>
2018-05-03 12:11:08 -0700

    DevCloud-Predict added

---
+++ projects/imaterialist_2018/DevCloud/README.md
@@ -0,0 +1,5 @@
+Upload model, trained weights and eval_dataset.h5 files, adjust model_path, trained_weights and test_file variables accordingly.
+
+run in DevCloud:
+
+qsub devcloud-predict
---
+++ projects/imaterialist_2018/DevCloud/devcloud-predict.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+from __future__ import print_function
+from __future__ import absolute_import
+
+
+# # iMatreialist - Predictor
+
+# ## TFNW
+
+# @alkari
+
+# # Predict and Create Submission file
+
+
+from keras.models import load_model
+
+import tensorflow as tf
+
+import numpy as np
+import pandas as pd
+import h5py, time, os
+from pathlib import Path
+
+size = 224
+
+start_time=time.time()
+
+def elapsed (start):
+    """
+    Returns elapsed time in hh:mm:ss format from start time in unix format
+    """
+    elapsed = time.time()-start
+    hours, rem = divmod(elapsed, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+home = str(Path.home())
+model_path = os.path.join(home,'nasnet/iMaterialist-model.h5')
+trained_weights = os.path.join(home,'nasnet/weights/NASNet-iMaterialist.h5')
+test_file = os.path.join(home,'nasnet/data/eval_dataset.h5')
+
+os.chdir(os.path.join(home,'nasnet'))
+
+if tf.device('/CPU:0'):
+  print('Loading model takes about 3 minutes...')
+  model=load_model(model_path)
+  print('Model loaded. Loading weights...')
+  model.load_weights(trained_weights)
+  print('Weights loaded.')
+
+def load_final_testset(filename,max=None):
+    with h5py.File(filename, 'r') as hf:
+        test_images = hf['test_dataset'][:max]        
+        # preprocess
+        test_images = test_images.astype('float32', copy=False)
+        test_images /= 127.5
+        test_images -=1 
+
+        missing = hf['missing'][:]
+    return test_images,missing
+
+# Load test set
+x_test_final,missing = load_final_testset(test_file)
+
+# Predict in 100 batches
+predictions = model.predict(x_test_final, verbose=1, batch_size=100)
+predictions = predictions[0].argmax(-1)
+
+# Adjust for files force labeled 0 to 128 and label missing files.
+arbitrary = 7 # or random!
+for i in range(predictions.shape[0]):
+    if predictions[i]==0:
+        predictions[i]=128
+        print('Changed image {} from 0 to {}'.format(i,predictions[i]))
+    if i in missing:
+        predictions[i]=arbitrary
+        print('Changed missing image {} prediction to {}'.format(i,predictions[i]))
+        
+def save_preds(predictions):
+    
+    df = pd.DataFrame({'id' : range(len(predictions)), 
+                       'predicted' : predictions})
+    df = df.drop(0) # Drop none existent image[0] prediction
+    os.makedirs('predictions', exist_ok=True)
+    filename = 'predictions/tfnw-preds_' + time.strftime('%Y%m%d_%H%M%S_%Z.csv')
+    df.to_csv(filename, index=False)
+    return(filename)
+
+filename = save_preds(predictions)
+
+
+for i in range(20):
+    print(i, predictions[i])
+    
+print('File <{}> saved.'.format(filename)
+
+print('Elapsed time: {}'.format(elapsed(start_time)))
